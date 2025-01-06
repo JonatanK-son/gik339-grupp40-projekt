32a148ccdde69d9fe9ee0e420bf5f4b447145b0e
JonatanK-son <kontra.kson@gmail.com>
2025-01-06 11:45:19 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,16 @@
-sqlite = require("sqlite3").verbose();
+const sqlite = require("sqlite3").verbose();
 const db = new sqlite.Database("./cars.db");
 
-express = require("express");
-server = express();
+const express = require("express");
+const server = express();
+
+server
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+
+    next();
+  });

1733f87fd3a5a3666aa63c69d899f7683f00cf6b
JonatanK-son <kontra.kson@gmail.com>
2025-01-06 11:30:18 +0100

    Update cars.sql

---
+++ server/cars.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS cars;
+CREATE TABLE IF NOT EXISTS cars(
+     id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT
+    ,brand  VARCHAR(12) NOT NULL
+    ,type   VARCHAR(8) NOT NULL
+    ,color  VARCHAR(8) NOT NULL
+);
+INSERT INTO cars(id, brand, type, color) VALUES (1, "Volvo", "Kombi", "Röd");
+
+select * from cars;
